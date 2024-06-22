f846593e28aa69312befe66fa63bf4cf4cd2df5f
TWintersww <134812478+TWintersww@users.noreply.github.com>
2024-06-22 14:08:27 -0700

    fixed get and post requests

--- part3/testing/index.js
+++ part3/testing/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const mongoose = require('mongoose')
+const Note = require('./models/note')
 
 const app = express()
 
@@ -9,55 +9,17 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
 
-const password = process.argv[2]
-const url = process.env.MONGODB_URL
-mongoose.set('strictQuery', false)
-mongoose.connect(url)
 
-const noteSchema = new mongoose.Schema({
-    content: String,
-    important: Boolean,
-})
-const Note = mongoose.model('Note', noteSchema)
 
-// let notes = [
-//     {
-//       id: 1,
-//       content: "HTML is easy",
-//       important: true
-//     },
-//     {
-//       id: 2,
-//       content: "Browser can execute only JavaScript",
-//       important: false
-//     },
-//     {
-//       id: 3,
-//       content: "GET and POST are the most important methods of HTTP protocol",
-//       important: true
-//     }
-// ]
-
-// app.get('/', (request, response) => {
-//     response.send('<h1>Hello World!</h1>')
-// })
 app.get('/api/notes', (request, response) => {
     Note.find({}).then(notes => {
         response.json(notes)
     })
 })
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => {
-        return note.id === id
-    })
-
-    if (note) {
+    Note.findById(request.params.id).then(note => {
         response.json(note)
-    }
-    else {
-        response.status(404).end()
-    }
+    })
 })
 app.delete('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
@@ -68,11 +30,6 @@ app.delete('/api/notes/:id', (request, response) => {
     response.status(204).end()
 })
 app.post('/api/notes', (request, response) => {
-    const maxId = notes.length > 0 
-        ? Math.max(...notes.map(note => note.id))
-        : 0
-    const newId = maxId + 1
-
     const body = request.body
     
     if (!body.content) {
@@ -81,18 +38,17 @@ app.post('/api/notes', (request, response) => {
         })
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
-        important: Boolean(body.important) || false,
-        id: newId,
-    }
+        important: body.important || false,
+    })
 
-    notes = notes.concat(note)
-
-    response.json(note)
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
