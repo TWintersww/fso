c91dd63eb073737fca396de359a3ae49c4389087
TWintersww <134812478+TWintersww@users.noreply.github.com>
2024-06-21 21:46:52 -0700

    connect /api/notes to mongo

--- part3/testing/index.js
+++ part3/testing/index.js
@@ -1,34 +1,50 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
+
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
 
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only JavaScript",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-    }
-]
+const password = process.argv[2]
+const url = process.env.MONGODB_URL
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
+const noteSchema = new mongoose.Schema({
+    content: String,
+    important: Boolean,
 })
+const Note = mongoose.model('Note', noteSchema)
+
+// let notes = [
+//     {
+//       id: 1,
+//       content: "HTML is easy",
+//       important: true
+//     },
+//     {
+//       id: 2,
+//       content: "Browser can execute only JavaScript",
+//       important: false
+//     },
+//     {
+//       id: 3,
+//       content: "GET and POST are the most important methods of HTTP protocol",
+//       important: true
+//     }
+// ]
+
+// app.get('/', (request, response) => {
+//     response.send('<h1>Hello World!</h1>')
+// })
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
 })
 app.get('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
@@ -80,4 +96,6 @@ const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
+
+
 
