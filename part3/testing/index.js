9e8f017f9a9731205dc57fc54df272d7f4138264
TWintersww <134812478+TWintersww@users.noreply.github.com>
2024-06-24 18:13:14 -0700

    integrate all http requests with mongo

--- part3/testing/index.js
+++ part3/testing/index.js
@@ -9,25 +9,45 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
 
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message)
 
+    if (error.name === 'CastError') {
+        return response.status(400).send({error: 'malformatted id'})
+    }
+
+    next(error)
+}
 
 app.get('/api/notes', (request, response) => {
     Note.find({}).then(notes => {
         response.json(notes)
     })
 })
-app.get('/api/notes/:id', (request, response) => {
-    Note.findById(request.params.id).then(note => {
-        response.json(note)
+app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id)
+    .then(note => {
+        if (note) {
+            response.json(note)
+        }
+        else {
+            response.status(404).end()
+        }
     })
+    .catch(error => next(error))
 })
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => {
-        return note.id !== id
-    })
-
-    response.status(204).end()
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndDelete(request.params.id)
+        .then(deletedPerson => {
+            if (deletedPerson) {
+                console.log('deleted', deletedPerson)
+            }
+            else {
+                console.log('tried to delete note that DNE')
+            }
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 app.post('/api/notes', (request, response) => {
     const body = request.body
@@ -47,6 +67,22 @@ app.post('/api/notes', (request, response) => {
         response.json(savedNote)
     })
 })
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+
+    const note = {
+        content: body.content,
+        important: body.important,
+    }
+
+    Note.findByIdAndUpdate(request.params.id, note, {new:true})
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
+})
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
